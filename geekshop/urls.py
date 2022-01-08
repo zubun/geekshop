46a0f76ea8bdff64ec475fe890011d29c07fb983
zubun <48867673+zubun@users.noreply.github.com>
2022-01-08 14:38:18 +0300

    Lesson 2 (#2)
    * lesson_1. Working static site
    
    * Lesson 2. Templates
    
    * Lesson 2. Repeat and filters

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 
 import mainapp.views as mainapp
 
-urlpatterns =[
+urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", mainapp.main),
-    path("products/", mainapp.products),
-    path("contact/", mainapp.contact),
+    path("", mainapp.main, name="main"),
+    path("products/", mainapp.products, name="products"),
+    path("contact/", mainapp.contact, name="contact"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,38 @@
+import datetime
+
 from django.shortcuts import render
 
 
 def main(request):
-    return render(request, "mainapp/index.html")
+    title = "главная"
+    products = [
+        {
+            "name": "Отличный стул",
+            "desc": "Расположитесь комфортно.",
+            "image_src": "product-1.jpg",
+            "image_href": "/product/1/",
+            "alt": "продукт 1",
+        },
+        {
+            "name": "Стул повышенного качества",
+            "desc": "Не оторваться.",
+            "image_src": "product-2.jpg",
+            "image_href": "/product/2/",
+            "alt": "продукт 2",
+        },
+    ]
+    content = {"title": title, "products": products}
+    return render(request, "mainapp/index.html", content)
 
 
 def products(request):
-    return render(request, "mainapp/products.html")
+    title = "продукты"
+    content = {"title": title}
+    return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
-    return render(request, "mainapp/contact.html")
+    title = "о нас"
+    visit_date = datetime.datetime.now()
+    content = {"title": title, "visit_date": visit_date}
+    return render(request, "mainapp/contact.html", content)
