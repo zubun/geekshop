160cd97905705bf7379db94e5897c56370412a4e
Vlad <zubun7@gmail.com>
2022-01-09 12:33:22 +0300

    Lesson 3. Djagno admin

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -14,3 +16,7 @@ urlpatterns = [
     path("products/classic", mainapp.products, name="products_classic"),
     path("contact/", mainapp.contact, name="contact"),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
