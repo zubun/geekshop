2d5873218795bfb04da1b72d0e9dc297892c74e6
Vlad <zubun7@gmail.com>
2022-01-09 12:45:31 +0300

    Lesson 3. Models in context

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,56 +1,35 @@
-import datetime
-
+from django.conf import settings
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Product, ProductCategory
 
 
 def main(request):
     title = "главная"
-    products = [
-        {
-            "name": "Отличный стул",
-            "desc": "Расположитесь комфортно.",
-            "image_src": "product-1.jpg",
-            "image_href": "/product/1/",
-            "alt": "продукт 1",
-        },
-        {
-            "name": "Стул повышенного качества",
-            "desc": "Не оторваться.",
-            "image_src": "product-2.jpg",
-            "image_href": "/product/2/",
-            "alt": "продукт 2",
-        },
-    ]
-    content = {"title": title, "products": products}
+
+    products = Product.objects.all()
+
+    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
 
 
 def products(request):
     title = "продукты"
-    links_menu = [
-        {"href": "products_all", "name": "все"},
-        {"href": "products_home", "name": "дом"},
-        {"href": "products_office", "name": "офис"},
-        {"href": "products_modern", "name": "модерн"},
-        {"href": "products_classic", "name": "классика"},
-    ]
-    same_products = [
-        {"name": "Отличный стул", "desc": "Не оторваться.", "image_src": "product-11.jpg", "alt": "продукт 11"},
-        {"name": "Стул повышенного качества", "desc": "Комфортно.", "image_src": "product-21.jpg", "alt": "продукт 21"},
-        {
-            "name": "Стул премиального качества",
-            "desc": "Просто попробуйте.",
-            "image_src": "product-31.jpg",
-            "alt": "продукт 31",
-        },
-    ]
-    content = {"title": title, "links_menu": links_menu, "same_products": same_products}
+    links_menu = ProductCategory.objects.all()
+    same_products = Product.objects.all()
+    content = {
+        "title": title,
+        "links_menu": links_menu,
+        "same_products": same_products,
+        "media_url": settings.MEDIA_URL,
+    }
     return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
     title = "о нас"
-    visit_date = datetime.datetime.now()
+    visit_date = timezone.now()
     locations = [
         {"city": "Москва", "phone": "+7-888-888-8888", "email": "info@geekshop.ru", "address": "В пределах МКАД"},
         {
