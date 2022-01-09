87133229bfcd2aa5759d47c353bf7fce98f07caa
zubun <48867673+zubun@users.noreply.github.com>
2022-01-09 10:20:22 +0300

    Lesson 2 (#3)
    * Lesson 2. Templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -27,12 +27,44 @@ def main(request):
 
 def products(request):
     title = "продукты"
-    content = {"title": title}
+    links_menu = [
+        {"href": "products_all", "name": "все"},
+        {"href": "products_home", "name": "дом"},
+        {"href": "products_office", "name": "офис"},
+        {"href": "products_modern", "name": "модерн"},
+        {"href": "products_classic", "name": "классика"},
+    ]
+    same_products = [
+        {"name": "Отличный стул", "desc": "Не оторваться.", "image_src": "product-11.jpg", "alt": "продукт 11"},
+        {"name": "Стул повышенного качества", "desc": "Комфортно.", "image_src": "product-21.jpg", "alt": "продукт 21"},
+        {
+            "name": "Стул премиального качества",
+            "desc": "Просто попробуйте.",
+            "image_src": "product-31.jpg",
+            "alt": "продукт 31",
+        },
+    ]
+    content = {"title": title, "links_menu": links_menu, "same_products": same_products}
     return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
     title = "о нас"
     visit_date = datetime.datetime.now()
-    content = {"title": title, "visit_date": visit_date}
+    locations = [
+        {"city": "Москва", "phone": "+7-888-888-8888", "email": "info@geekshop.ru", "address": "В пределах МКАД"},
+        {
+            "city": "Екатеринбург",
+            "phone": "+7-777-777-7777",
+            "email": "info_yekaterinburg@geekshop.ru",
+            "address": "Близко к центру",
+        },
+        {
+            "city": "Владивосток",
+            "phone": "+7-999-999-9999",
+            "email": "info_vladivostok@geekshop.ru",
+            "address": "Близко к океану",
+        },
+    ]
+    content = {"title": title, "visit_date": visit_date, "locations": locations}
     return render(request, "mainapp/contact.html", content)
