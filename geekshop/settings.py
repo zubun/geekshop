7d3c322ac21fc3ed1378048fe6de1f29489ba210
Vladislav Zubunov <zubun7@gmail.com>
2022-01-15 14:26:33 +0300

    Lesson 5. Basket app start

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -34,14 +34,15 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "mainapp",
-    "authnapp",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'mainapp',
+    'authnapp',
+    'basketapp',
 ]
 
 # Auth model
--- geekshop/urls.py
+++ geekshop/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path("products/", include("mainapp.urls", namespace="products")),
     path("contact/", mainapp.contact, name="contact"),
     path("auth/", include("authnapp.urls", namespace="auth")),
+    path("basket/", include("basketapp.urls", namespace="basket")),
 ]
 
 
