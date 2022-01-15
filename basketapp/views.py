6fb91d49ed36e90c8861b809517d493a6502e4f0
Vladislav Zubunov <zubun7@gmail.com>
2022-01-15 16:35:46 +0300

    Lesson 6. Basket read and delete

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import HttpResponseRedirect, get_object_or_404, render
 
 from basketapp.models import Basket
@@ -5,9 +6,10 @@ from mainapp.models import Product
 
 
 def basket(request):
-    # content = {}
-    # return render(request, "basketapp/basket.html", content)
-    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+    title = "корзина"
+    basket_items = Basket.objects.filter(user=request.user).order_by("product__category")
+    content = {"title": title, "basket_items": basket_items, "media_url": settings.MEDIA_URL}
+    return render(request, "basketapp/basket.html", content)
 
 
 def basket_add(request, pk):
@@ -23,7 +25,7 @@ def basket_add(request, pk):
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
 
-def basket_remove(request):
-    # content = {}
-    # return render(request, "basketapp/basket.html", content)
+def basket_remove(request, pk):
+    basket_record = get_object_or_404(Basket, pk=pk)
+    basket_record.delete()
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
