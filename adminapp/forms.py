2410c15fc8caba7de318f125b9fdec26d3faa427
Vladislav Zubunov <zubun7@gmail.com>
2022-01-20 23:23:59 +0300

    lesson_8. CRUD products.

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 from authnapp.forms import ShopUserEditForm
 from authnapp.models import ShopUser
-from mainapp.models import ProductCategory
+from mainapp.models import Product, ProductCategory
 
 
 class ShopUserAdminEditForm(ShopUserEditForm):
@@ -20,4 +20,16 @@ class ProductCategoryEditForm(forms.ModelForm):
 
     class Meta:
         model = ProductCategory
+        fields = "__all__"
+
+
+class ProductEditForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ProductEditForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
+            field.help_text = ""
+
+    class Meta:
+        model = Product
         fields = "__all__"
--- adminapp/views.py
+++ adminapp/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import HttpResponseRedirect, get_object_or_404, redirect, render
 from django.urls import reverse
 
-from adminapp.forms import ProductCategoryEditForm, ShopUserAdminEditForm
+from adminapp.forms import ProductCategoryEditForm, ProductEditForm, ShopUserAdminEditForm
 from authnapp.forms import ShopUserRegisterForm
 from authnapp.models import ShopUser
 from mainapp.models import Product, ProductCategory
@@ -144,21 +144,63 @@ def products(request, pk):
     return render(request, "adminapp/products.html", content)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_create(request, pk):
-    response = redirect("admin:categories")
-    return response
+    title = "продукт/создание"
+    category = get_object_or_404(ProductCategory, pk=pk)
+
+    if request.method == "POST":
+        product_form = ProductEditForm(request.POST, request.FILES)
+        if product_form.is_valid():
+            product_form.save()
+            return HttpResponseRedirect(reverse("admin:products", args=[pk]))
+    else:
+        # set initial value for form
+        product_form = ProductEditForm(initial={"category": category})
+
+    content = {"title": title, "update_form": product_form, "category": category, "media_url": settings.MEDIA_URL}
+    return render(request, "adminapp/product_update.html", content)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_read(request, pk):
-    response = redirect("admin:categories")
-    return response
+    title = "продукт/подробнее"
+    product = get_object_or_404(Product, pk=pk)
+    content = {"title": title, "object": product, "media_url": settings.MEDIA_URL}
+    return render(request, "adminapp/product_read.html", content)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_update(request, pk):
-    response = redirect("admin:categories")
-    return response
+    title = "продукт/редактирование"
+    edit_product = get_object_or_404(Product, pk=pk)
+
+    if request.method == "POST":
+        edit_form = ProductEditForm(request.POST, request.FILES, instance=edit_product)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse("admin:product_update", args=[edit_product.pk]))
+    else:
+        edit_form = ProductEditForm(instance=edit_product)
+
+    content = {
+        "title": title,
+        "update_form": edit_form,
+        "category": edit_product.category,
+        "media_url": settings.MEDIA_URL,
+    }
+    return render(request, "adminapp/product_update.html", content)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_delete(request, pk):
-    response = redirect("admin:categories")
-    return response
+    title = "продукт/удаление"
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == "POST":
+        product.is_active = False
+        product.save()
+        return HttpResponseRedirect(reverse("admin:products", args=[product.category.pk]))
+
+    content = {"title": title, "product_to_delete": product, "media_url": settings.MEDIA_URL}
+    return render(request, "adminapp/product_delete.html", content)
