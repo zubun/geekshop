9e805a218c2840ea06c45946592e36c4f02eb9f3
zubun <48867673+zubun@users.noreply.github.com>
2022-01-26 22:26:51 +0300

    Lesson 8 (#9)
    * lesson_8. CRUD products.
    
    * lesson_8. Paginator.
    
    * lesson_8. CBV.
    
    * lesson_8. Template filters. The END.
    
    * Lesson 8. Code style fix

--- mainapp/models.py
+++ mainapp/models.py
@@ -18,6 +18,7 @@ class Product(models.Model):
     description = models.TextField(verbose_name="описание продукта", blank=True)
     price = models.DecimalField(verbose_name="цена продукта", max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name="количество на складе", default=0)
+    is_active = models.BooleanField(verbose_name="продукт активен", default=True)
 
     def __str__(self):
         return f"{self.name} ({self.category.name})"
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,7 @@
 import random
 
 from django.conf import settings
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
@@ -11,9 +12,7 @@ from .models import Contact, Product, ProductCategory
 
 def main(request):
     title = "главная"
-
-    products = Product.objects.all()[:4]
-
+    products = Product.objects.filter(is_active=True, category__is_active=True)[:3]
     content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
 
@@ -26,32 +25,43 @@ def get_basket(user):
 
 
 def get_hot_product():
-    products = Product.objects.all()
+    products = Product.objects.filter(is_active=True, category__is_active=True)
     return random.sample(list(products), 1)[0]
 
 
 def get_same_products(hot_product):
-    same_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+    same_products = Product.objects.filter(category=hot_product.category, is_active=True).exclude(pk=hot_product.pk)[:3]
     return same_products
 
 
-def products(request, pk=None):
+def products(request, pk=None, page=1):
     title = "продукты"
-    links_menu = ProductCategory.objects.all()
+    links_menu = ProductCategory.objects.filter(is_active=True)
     basket = get_basket(request.user)
 
     if pk is not None:
         if pk == 0:
-            products = Product.objects.all().order_by("price")
-            category = {"name": "все"}
+            category = {"pk": 0, "name": "все"}
+            products = Product.objects.filter(is_active=True, category__is_active=True).order_by("price")
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
-            products = Product.objects.filter(category__pk=pk).order_by("price")
+            products = Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by(
+                "price"
+            )
+
+        paginator = Paginator(products, 2)
+        try:
+            products_paginator = paginator.page(page)
+        except PageNotAnInteger:
+            products_paginator = paginator.page(1)
+        except EmptyPage:
+            products_paginator = paginator.page(paginator.num_pages)
+
         content = {
             "title": title,
             "links_menu": links_menu,
             "category": category,
-            "products": products,
+            "products": products_paginator,
             "media_url": settings.MEDIA_URL,
             "basket": basket,
         }
@@ -69,21 +79,21 @@ def products(request, pk=None):
     return render(request, "mainapp/products.html", content)
 
 
+def product(request, pk):
+    title = "продукты"
+    content = {
+        "title": title,
+        "links_menu": ProductCategory.objects.filter(is_active=True),
+        "product": get_object_or_404(Product, pk=pk),
+        "basket": get_basket(request.user),
+        "media_url": settings.MEDIA_URL,
+    }
+    return render(request, "mainapp/product.html", content)
+
+
 def contact(request):
     title = "о нас"
     visit_date = timezone.now()
     locations = Contact.objects.all()
     content = {"title": title, "visit_date": visit_date, "locations": locations}
     return render(request, "mainapp/contact.html", content)
-
-
-def product(request, pk):
-    title = "продукты"
-    content = {
-        "title": title,
-        "links_menu": ProductCategory.objects.all(),
-        "product": get_object_or_404(Product, pk=pk),
-        "basket": get_basket(request.user),
-        "media_url": settings.MEDIA_URL,
-    }
-    return render(request, "mainapp/product.html", content)
