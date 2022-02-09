f08a43c2983574878d9deaf08ca7cdad94fc6a04
zubun <zubun7@gmail.com>
2022-02-09 21:43:58 +0300

    Lesson 11 (#12)
    * lesson_11. Dropdown
    
    * Lesson 11. Code style fix

--- basketapp/models.py
+++ basketapp/models.py
@@ -28,3 +28,7 @@ class Basket(models.Model):
         _items = Basket.objects.filter(user=self.user)
         _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
         return _totalcost
+
+    @staticmethod
+    def get_items(user):
+        return Basket.objects.filter(user=user).order_by("product__category")
